19bd9a4fe67916f1d411b3ad0696560acbbc8508
clayton.santos <clayton.santos@callflex.local>
2022-02-26 20:32:02 -0300

    03 - Listar os Planos do LaraFood

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -7,5 +7,35 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="card">
+        <div class="card-header">
+            filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th class="col-1">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>
+                                {{ $plan->name }}
+                            </td>
+                            <td>
+                                {{$plan->price}}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-warning">VER</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
